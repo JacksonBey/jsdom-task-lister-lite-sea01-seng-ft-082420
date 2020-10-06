f4685a046735f0352ec35b1c9f9196ed212be08b
JacksonBey <jacksongrowson@gmail.com>
2020-10-06 13:14:08 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //Defining a listener for our button, specifically, an onclick handler
+  document.getElementById("add").onclick = function(event) {
+    //First things first, we need our text:
+    let text = document.getElementById("new-task-description").value; //.value gets input values
+    //Now construct a quick list element
+    let li = document.createElement("li");
+    li.innerText = text
+    //Now use appendChild and add it to the list!
+    let ul = document.getElementById("tasks")
+    // document.getElementById("tasks").appendChild(li);
+    ul.appendChild(li);
+    event.preventDefault()
+  }
 });
